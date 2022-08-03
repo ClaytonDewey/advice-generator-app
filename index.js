01ffdd59d5a7adb1da8f609b7b7ace2def97cb0f
ClaytonDewey <clay@dryadmedia.com>
2022-08-03 14:13:43 -0500

    Move API call to function.

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ const adviceId = document.querySelector(".id");
 const adviceText = document.querySelector("blockquote");
 const button = document.querySelector("button");
 
-fetch("	https://api.adviceslip.com/advice")
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(res);
-    }
-  })
-  .then((data) => {
-    const { advice, id } = data.slip;
-    adviceId.innerText = id;
-    adviceText.innerHTML = `&ldquo;${advice}&rdquo;`;
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+button.addEventListener("click", generateAdvice);
+window.addEventListener("load", generateAdvice);
 
-button.addEventListener("click", () => {
-  location.reload();
-});
+function generateAdvice() {
+  fetch("	https://api.adviceslip.com/advice")
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return Promise.reject(res);
+      }
+    })
+    .then((data) => {
+      const { advice, id } = data.slip;
+      adviceId.innerText = id;
+      adviceText.innerHTML = `&ldquo;${advice}&rdquo;`;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
